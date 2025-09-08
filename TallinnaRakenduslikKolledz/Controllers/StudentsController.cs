c1d5ed031b2d004f3cf08e69b42eba989a299fb7
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-09-08 15:01:48 +0300

    Added student create view

---
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind
+            ("ID,LastName,FirstName,EnrollmentDate,IdCode")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+    }
+}
--- TallinnaRakenduslikKolledz/Models/Student.cs
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -6,7 +6,7 @@ namespace TallinnaRakenduslikKolledz.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         
         public string LastName { get; set; }
         public DateTime EnrollmentDate { get; set; }
