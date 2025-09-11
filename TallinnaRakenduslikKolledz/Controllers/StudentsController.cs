0aeb3c5d9acd7c012a4429a73efd9e26d3be5a78
Opilane TTHK <opilane@tthk.ee>
2025-09-11 14:01:47 +0300

    np tund
    np tund

--- TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -37,5 +37,49 @@ namespace TallinnaRakenduslikKolledz.Controllers
             }
             return View(student);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m  => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m  => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
