aaab4514a49ecead78ff9cbb08e0f723c8e82813
Leib92 <opilane@HP-CZC321705Q>
2025-10-10 18:06:36 +0300

    Konsultatsioon
    k

--- TallinnaRakenduslikKolledz/Controllers/InstructorsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/InstructorsController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
 
 namespace TallinnaRakenduslikKolledz.Controllers
 {
     public class InstructorsController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+        public InstructorsController(SchoolContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? Id, int? CourseId)
+        {
+            var vm = new InstructorIndexData();
+            vm.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments).ToListAsync();
+            return View(vm);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var instructor = new Instructor();
+            instructor.CourseAssignments = new List<CourseAssignment>();
+            return View(instructor);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Instructor instructor, string selectedcourses)
+        {
+            if (selectedcourses != null)
+            {
+                instructor.CourseAssignments = new List<CourseAssignment>();
+                foreach (var course in selectedcourses)
+                {
+                    var courseToAdd = new CourseAssignment 
+                    {
+                        InstructorID = instructor.ID,
+                        CourseID = course
+                    };
+                    instructor.CourseAssignments.Add(courseToAdd);
+                }
+            }
+            ModelState.Remove("selectedcourses");
+            if (ModelState.IsValid)
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(instructor);
         }
     }
 }
