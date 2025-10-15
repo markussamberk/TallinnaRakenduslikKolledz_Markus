db076758cffac4759e7142945b77803223f7ef67
markussamberk <162631294+markussamberk@users.noreply.github.com>
2025-10-15 22:53:16 +0300

    Add files via upload

---
+++ TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledz.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+
+        /*sugu, isikukood, vanus */
+
+        public string? Gender { get; set; }
+
+        public int? IdCode { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
