9ade2563ca71d2d5287b23a31f2fdf315bbd354d
markussamberk <markussamberk@gmail.com>
2025-12-03 22:08:46 +0200

    fixed merge conflicts

--- TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Program.cs
+++ TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Program.cs
@@ -1,65 +1,3 @@
-<<<<<<< HEAD:TallinnaRakenduslikKolledz/Program.cs
-using Microsoft.EntityFrameworkCore;
-using TallinnaRakenduslikKolledz.Data;
-
-namespace TallinnaRakenduslikKolledz
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            var app = builder.Build();
-            CreateDbIfNotExists(app);
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-
-        private static void CreateDbIfNotExists(IHost app)
-        {
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<SchoolContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "Error occurred on creating DB");
-                }
-            }
-        }
-    }
-}
-=======
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
 
@@ -101,4 +39,3 @@ namespace TallinnaRakenduslikKolledz
         }
     }
 }
->>>>>>> db076758cffac4759e7142945b77803223f7ef67:TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Program.cs
