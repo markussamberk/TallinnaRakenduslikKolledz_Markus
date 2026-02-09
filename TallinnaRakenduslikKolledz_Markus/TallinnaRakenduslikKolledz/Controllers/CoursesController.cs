4e273279b0337fb5611a959d4348942721836e7d
markussamberk <markussamberk@gmail.com>
2026-02-09 21:06:18 +0200

    Courses Create + Edit

--- TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
+++ TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
 
 namespace TallinnaRakenduslikKolledz.Controllers
 {
@@ -16,6 +18,68 @@ namespace TallinnaRakenduslikKolledz.Controllers
             var courses = await _context.Courses.Include(c => c.Department).AsNoTracking().ToListAsync();
 
             return View(courses);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            DepartmentsDropDownList();
+            ViewData["SelectedAction"] = "Create";
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+        private void DepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var department = from x in _context.Departments orderby x.Name select x;
+
+            ViewBag.DepartmentID = new SelectList(department.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            DepartmentsDropDownList(course.DepartmentID);
+            ViewData["SelectedAction"] = "Edit";
+            return View("Create", course);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> ConfirmEdit([Bind("CourseId, Title, Credits, DepartmentID")] Course course)
+        {
+            if (course.CourseId == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Courses.Update(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View("Create", course);
         }
     }
 }
