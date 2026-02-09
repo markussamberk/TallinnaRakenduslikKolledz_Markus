cc8054c4003daec063f84347c67fb7c3062d6958
markussamberk <markussamberk@gmail.com>
2026-02-09 20:29:51 +0200

    Course index

---
+++ TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+        public CoursesController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var courses = await _context.Courses.Include(c => c.Department).AsNoTracking().ToListAsync();
+
+            return View(courses);
+        }
+    }
+}
--- TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Models/Course.cs
+++ TallinnaRakenduslikKolledz_Markus/TallinnaRakenduslikKolledz/Models/Course.cs
@@ -5,7 +5,7 @@
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
-        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
         public Department? Department { get; set; }
         public int? DepartmentID { get; set; }
         public ICollection<CourseAssignment>? CourseAssignments { get; set; }
